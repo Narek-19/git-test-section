90d9d4181a6fc4ffb9210b887ee4190c89327b89
ArtyomPC <artyom@skipti.com>
2022-04-18 18:50:59 +0400

    Revert "first2 commit"
    This reverts commit 62b4d8c0eb380def61a9b8ee5cb106064bc75295.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ function App() {
       <h2>Header options</h2>
       <h3>second Brancch</h3>
       <h4>Third Branch</h4>
-      {/* comment */}
-      <h1>Git second commit1</h1>
-      <h2>Header options1</h2>
-      <h3>second Brancch1</h3>
-      <h4>Third Branch1</h4>
     </div>
   );
 }
