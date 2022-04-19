dd76960931baaa8b95c8bed7d6d8864f3c2fbd93
ArtyomPC <artyom@skipti.com>
2022-04-19 18:38:39 +0400

    changes in foter

---
+++ src/Component/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+export const Footer =()=>{
+    const fotter = "Footer";
+    if(fotter){
+        console.log("a");
+
+    }else {
+        console.log('No footer');
+    }
+    return (
+        <div>
+            <h1>Footer</h1>
+        </div>
+    )
+}
